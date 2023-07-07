3d7308cee75d7173015305629bdb25f6c9f63b7d
missael-777 <missael.length.212@gmail.com>
2023-07-06 20:07:16 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const data = [
+	{
+		"name": "Odysseus Cochran",
+		"country": "Sweden",
+		"email": "a@protonmail.edu",
+		"postalZip": "A8J 6L8"
+	},
+	{
+		"name": "Carla Conley",
+		"country": "United States",
+		"email": "ligula.tortor.dictum@yahoo.net",
+		"postalZip": "78-84"
+	},
+	{
+		"name": "Galena Hayes",
+		"country": "Ukraine",
+		"email": "viverra@outlook.couk",
+		"postalZip": "4734 XU"
+	}
+];
+
+data.forEach( dato => {
+    console.log(dato);
+});
